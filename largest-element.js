fde1bb17caa27a45d3e1e9de3a585e8d382dd270
yousufrahman <yousufrahman5@gmail.com>
2021-09-12 00:46:43 +0600

    sorry me ,i am don't degre it ,thankd for it

--- largest-element.js
+++ largest-element.js
@@ -1,18 +1,37 @@
-function largestElement(numbers) {
-    let largest = numbers[0];
-    for (let i = 0; i < numbers.length; i++) {
-        const element = numbers[i];
-        if (element > largest) {
-            largest = element;
-        }
+// function largestElement(numbers) {
+//     let largest = numbers[0];
+//     for (let i = 0; i < numbers.length; i++) {
+//         const element = numbers[i];
+//         if (element > largest) {
+//             largest = element;
+//         }
 
-    }
-    return largest;
+//     }
+//     return largest;
 
+// }
+// const ages = [12, 54, 2, 75, 32, 40];
+// const oldest = largestElement(ages);
+// const oldest2 = largestElement([-10, -15, -34, -56]);
+
+// console.log('odest2', oldest2)
+// // console.log('odest', oldest);
+
+
+// // Task: find the lowest element of an array 
+// function min(input) {
+//     if (toString.call(input) !== "[object Array]");
+//     return false;
+//     return Math.min.apply(null, input);
+// }
+// console.log(min([12, 32, 53, 14]));
+// console.log(min([-12, -43 - 53 - 5 - 4]));
+function min(input) {
+    if (toString.call(input) !== "[object Array]")
+        return false;
+    return Math.min.apply(null, input);
 }
-const ages = [12, 54, 2, 75, 32, 40];
-const oldest = largestElement(ages);
-const oldest2 = largestElement([-10, -15, -34, -56]);
 
-console.log('odest2', oldest2)
-// console.log('odest', oldest);
+console.log(min([12, 34, 56, 1]));
+console.log(min([-12, -34, 0, -56, -1]));
+
